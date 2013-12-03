277b9b21ac82d4b8e596803e69f6847aacdc14ef
muten84 <bifulcoluigi@gmail.com>
2013-12-04 00:32:31 +0100

    nunchuck constants utility class

---
+++ wiimod-nunchuck/src/main/java/it/luigibifulco/pi/nunchuck/NunchuckCostants.java
@@ -0,0 +1,18 @@
+package it.luigibifulco.pi.nunchuck;
+
+public interface NunchuckCostants {
+
+	public final static int MIN_JX = 22;
+	public final static int MAX_JX = 223;
+	public final static int MIN_JY = 0;
+	public final static int MAX_JY = 255;
+	public final static int MIN_AX = 0;
+	public final static int MAX_AX = 1024;
+	public final static int MIN_AY = 0;
+	public final static int MAX_AY = 1024;
+	public final static int MIN_AZ = 0;
+	public final static int MAX_AZ = 1024;
+
+	public final static int JX_STATIONARY = 114;
+
+}
